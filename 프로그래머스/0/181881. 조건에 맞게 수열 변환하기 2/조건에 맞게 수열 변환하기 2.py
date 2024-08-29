349838dbf8f836a8570ef252734143e1b36711ee
kivccc <71814049+kivccc@users.noreply.github.com>
2024-08-30 03:04:37 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 636.76 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -2,12 +2,17 @@ def solution(arr):
     answer=0
     while True:
         tmp=arr.copy()
+        flag=False
         for idx,val in enumerate(arr):
             if(val>=50 and val %2 ==0 ):
                 arr[idx]=arr[idx]//2
+                flag=True
             elif(val<50 and val% 2):
-                arr[idx]=arr[idx]*2+1         
+                arr[idx]=arr[idx]*2+1   
+                flag=True
         answer+=1
+        if(flag):
+            continue
         if(tmp==arr):
             return answer-1
         
