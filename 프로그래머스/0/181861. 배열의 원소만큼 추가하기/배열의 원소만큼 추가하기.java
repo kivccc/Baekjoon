cc8654eeb79b74d756f917f9116f4d88a46d1b51
kivccc <71814049+kivccc@users.noreply.github.com>
2024-10-04 22:32:34 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 5.03 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> answer=new ArrayList<>();
+        for(int i:arr){
+            for(int j=0;j<i;j++)
+                answer.add(i);
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
