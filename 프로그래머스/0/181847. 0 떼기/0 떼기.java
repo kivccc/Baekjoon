898f0e421b30b5113d02313bb4f4df4e539c2d04
kivccc <71814049+kivccc@users.noreply.github.com>
2024-10-05 21:45:03 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        for(int i=0;i<n_str.length();i++)
+            if(n_str.charAt(i)!='0')
+                return n_str.substring(i,n_str.length());
+        return "";
+    }
+}
