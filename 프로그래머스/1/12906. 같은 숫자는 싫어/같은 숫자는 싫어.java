7e27bbcc9b895523032dd69ac88006026e050165
kivccc <71814049+kivccc@users.noreply.github.com>
2024-10-22 17:52:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 42.54 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(arr[0]);
+        for(int i=1;i<arr.length;i++){
+            if(list.get(list.size()-1)!=arr[i])
+                list.add(arr[i]);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
