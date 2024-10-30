282505f121c869cb580501c2dca803af1d0f360b
kivccc <71814049+kivccc@users.noreply.github.com>
2024-10-31 02:31:21 +0900

    [level 2] Title: n^2 배열 자르기, Time: 12.70 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, long left, long right) {
+
+        int[] answer=new int[(int)(right - left + 1)];
+        int idx=0;
+        for(long i=left;i<=right;i++){
+            if(i%n<=i/n)
+                answer[idx++]=(int)(i/n+1);
+            else
+                answer[idx++]=(int)(i%n+1);
+        }
+        return answer;
+    }
+}
