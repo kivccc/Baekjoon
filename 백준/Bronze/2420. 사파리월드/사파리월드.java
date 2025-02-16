b0eb48764bd71a72c473cedaf01f0f499c405914
kivccc <71814049+kivccc@users.noreply.github.com>
2025-02-16 22:24:18 +0900

    [Bronze V] Title: 사파리월드, Time: 168 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        Long a=sc.nextLong();
+        Long b=sc.nextLong();
+        System.out.println(Math.abs(a-b));
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st=new StringTokenizer(br.readLine());
+        //int N=Integer.parseInt(st.nextToken());
+	}
+}
