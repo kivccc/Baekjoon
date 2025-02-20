8b5402a1eabf71119e668102a7cfe77b65fa6524
kivccc <71814049+kivccc@users.noreply.github.com>
2025-02-20 16:47:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+        int[] arr=new int[11];
+        arr[1]=1;
+        arr[2]=2;
+        arr[3]=4;
+        for(int i=4;i<=10;i++)
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<T;i++){
+            int n=Integer.parseInt(br.readLine());
+            sb.append(arr[n]).append("\n");
+        }
+        System.out.println(sb.toString());
+	}
+}
