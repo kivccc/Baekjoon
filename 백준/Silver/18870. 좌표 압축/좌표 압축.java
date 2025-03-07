0549083576c38d79e4d495891e9c33492352db11
kivccc <71814049+kivccc@users.noreply.github.com>
2025-03-07 13:34:03 +0900

    [Silver II] Title: 좌표 압축, Time: 2160 ms, Memory: 343036 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,31 +2,35 @@ import java.util.*;
 import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
-        Scanner sc=new Scanner(System.in);
-        int N=sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
         int[] arr=new int[N];
+        
         ArrayList<Integer> list=new ArrayList<>();
         HashSet<Integer> set=new HashSet<>();
         HashMap<Integer,Integer> map=new HashMap<>();
+        
+        StringTokenizer st=new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
-            int val=sc.nextInt();
+            int val=Integer.parseInt(st.nextToken());
             arr[i]=val;
             if(!set.contains(val)){
                 list.add(val);
                 set.add(val);
             }
         }
+        
         Collections.sort(list);
         for (int i = list.size() - 1; i >= 0; i--) {
             map.put(list.get(i),i);
         }
+        
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<N;i++){
             sb.append(map.get(arr[i])).append(" ");
         }
         System.out.println(sb.toString());
-        //StringTokenizer st=new StringTokenizer(br.readLine());
-        //int N=Integer.parseInt(st.nextToken());
+
         //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	}
 }
