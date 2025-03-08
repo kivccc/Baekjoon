e5984ae1c7666c0609263a18e04c05af2003523e
kivccc <71814049+kivccc@users.noreply.github.com>
2025-03-08 22:23:35 +0900

    [Bronze V] Title: A+B - 2, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        System.out.println(A+B);
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //int N=Integer.parseInt(br.readLine());
+
+        /*
+        for(int i=0;i<N;i++){
+        
+        }
+        */
+        
+        //StringTokenizer st=new StringTokenizer(br.readLine());
+        //int N=Integer.parseInt(st.nextToken());
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+}
