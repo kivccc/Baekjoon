37c78b5aecadc9421b43a8e4bbe38d7392d9a9bf
kivccc <71814049+kivccc@users.noreply.github.com>
2025-03-26 22:37:59 +0900

    [Silver III] Title: N과 M (5), Time: 320 ms, Memory: 29216 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static int[] ans;
+    public static boolean[] visit;
+    public static StringBuilder sb=new StringBuilder();
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        arr=new int[N];
+        visit=new boolean[N];
+        ans=new int[M];
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+    
+    public static void dfs(int dep){
+        if(dep==M){
+            for(int val:ans)
+                sb.append(val).append(" ");
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i=0;i<N;i++){
+            if(!visit[i]){
+                visit[i]=true;
+                ans[dep]=arr[i];
+                dfs(dep+1);    
+                visit[i]=false;
+            }
+            
+        }
+        
+    }
+}
