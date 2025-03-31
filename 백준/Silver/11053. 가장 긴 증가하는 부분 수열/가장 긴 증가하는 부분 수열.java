b739b47fd9447b3e1d428bae260b03483ba5852e
kivccc <71814049+kivccc@users.noreply.github.com>
2025-04-01 00:38:32 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 136 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int[] arr=new int[N];
+        int[] mem=new int[N];
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        for(int i=0;i<N;i++){
+            mem[i]=1;
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i] && mem[i]<mem[j]+1)
+                    mem[i]=mem[j]+1;
+            }
+        }
+        int max=0;
+        for(int i:mem)
+            max=Math.max(max,i);
+        System.out.println(max);
+	}
+}
