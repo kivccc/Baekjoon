26e25f99155daee119670f3a09633330ef46a4b2
kivccc <71814049+kivccc@users.noreply.github.com>
2025-05-03 18:36:33 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 636 ms, Memory: 136844 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int[] arr=new int[N];
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        for(int i:arr){
+            int idx=Collections.binarySearch(list,i);
+            if(idx<0)
+                idx=-idx-1;
+            if(idx==list.size()){
+                list.add(i);
+            }
+            else
+                list.set(idx,i);
+        }
+        System.out.println(list.size());
+	}
+}
