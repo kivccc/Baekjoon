c7461069894bf51cba7152c5960e50792e15413c
kivccc <71814049+kivccc@users.noreply.github.com>
2025-05-16 15:30:20 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 488 ms, Memory: 51044 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -3,80 +3,73 @@ import java.io.*;
 public class Main {
     public static class Node{
         int idx;
-        int dist;
-        public Node(int idx,int dist){
+        int cost;
+        public Node(int idx,int cost){
             this.idx=idx;
-            this.dist=dist;
-        } 
+            this.cost=cost;
+        }
     }
     public static int n;
     public static int m;
     public static ArrayList<Node>[] graph;
-    public static int[] answer;
+    public static int[] dist;
     public static int start;
     public static int end;
+    
 	public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n=Integer.parseInt(br.readLine());
-        graph=new ArrayList[n+1];
-        for(int i=1;i<=n;i++)
-            graph[i]=new ArrayList<>();
-        answer=new int[n+1];
         m=Integer.parseInt(br.readLine());
+        graph=new ArrayList[n+1];
+        dist=new int[n+1];
+        for(int i=1;i<=n;i++){
+            dist[i]=1000000000;
+            graph[i]=new ArrayList<>();
+        }
         for(int i=0;i<m;i++){
             StringTokenizer st=new StringTokenizer(br.readLine());
-            int a=Integer.parseInt(st.nextToken());
-            int b=Integer.parseInt(st.nextToken());
-            int c=Integer.parseInt(st.nextToken());
-            graph[a].add(new Node(b,c));
+            int from=Integer.parseInt(st.nextToken());
+            int to=Integer.parseInt(st.nextToken());
+            int cost=Integer.parseInt(st.nextToken());
+            graph[from].add(new Node(to,cost));
         }
+        
         StringTokenizer st=new StringTokenizer(br.readLine());
         start=Integer.parseInt(st.nextToken());
         end=Integer.parseInt(st.nextToken());
-        StringBuilder sb=new StringBuilder();
-        sb.append(dijk(start)).append("\n");
-        Stack<Integer> path=new Stack<>();
-        int cur=end;
-        while(cur!=-1){
-            path.push(cur);
-            cur=answer[cur];
-        }
-        sb.append(path.size()).append("\n");
-        while(!path.isEmpty())
-            sb.append(path.pop()).append(" ");
-        System.out.println(sb);
-        //
-        //Scanner sc=new Scanner(System.in);
-        //int N=sc.nextInt();
-        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        dijk();
 	}
-    
-    public static int dijk(int start){
-        int[] distance=new int[n+1];
-        for(int i=1;i<=n;i++){
-            distance[i]=1000000000;
-            answer[i]=-1;
-        }
-        distance[start]=0;
+    public static void dijk(){
+        int[] prev=new int[n+1]; 
+        Arrays.fill(prev,-1);
         PriorityQueue<Node> pq=new PriorityQueue<>((e1,e2)->{
-            return e1.dist-e2.dist;
+            return Integer.compare(e1.cost,e2.cost);
         });
-        
+        dist[start]=0;
         pq.add(new Node(start,0));
-        
         while(!pq.isEmpty()){
             Node now=pq.poll();
-            if(distance[now.idx]<now.dist)
-                continue;
-            for(Node next:graph[now.idx]){
-                if(distance[now.idx]+next.dist<distance[next.idx]){
-                    answer[next.idx]=now.idx;
-                    distance[next.idx]=distance[now.idx]+next.dist;
-                    pq.add(new Node(next.idx,distance[next.idx]));
+            int nowidx=now.idx;
+            int nowcost=now.cost;
+            if (dist[nowidx] < nowcost) continue;
+            for(Node to:graph[nowidx]){
+                if(dist[to.idx]>dist[nowidx]+to.cost){
+                    dist[to.idx]=dist[nowidx]+to.cost;
+                    prev[to.idx]=nowidx;
+                    pq.add(new Node(to.idx,dist[to.idx]));
                 }
             }
         }
-        return distance[end];
+        System.out.println(dist[end]);
+        ArrayList<Integer> path=new ArrayList<>();
+        for(int i=end;i!=-1;i=prev[i])
+            path.add(i);
+        StringBuilder sb=new StringBuilder();
+        for(int i=path.size()-1;i>=0;i--)
+            sb.append(path.get(i)).append(" ");
+        System.out.println(path.size());
+        System.out.println(sb);
     }
 }
