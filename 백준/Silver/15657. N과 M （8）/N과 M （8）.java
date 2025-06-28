0a58ddac7a6650d3afe5b727d1760f158c493a29
kivccc <71814049+kivccc@users.noreply.github.com>
2025-06-28 14:16:21 +0900

    [Silver III] Title: N과 M (8), Time: 140 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[] arr;
+    public static int[] mem;
+    public static boolean[] visited;
+    public static StringBuilder sb=new StringBuilder();
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        mem=new int[m];
+        st=new StringTokenizer(br.readLine());
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<n;i++){
+            set.add(Integer.parseInt(st.nextToken()));
+            //System.out.println(set);
+        }
+        n=set.size();
+        arr=new int[n];
+        visited=new boolean[n];
+        int idx=0;
+        for(int i:set)
+            arr[idx++]=i;
+        Arrays.sort(arr);
+        dfs(0,0);
+        System.out.println(sb);
+          
+        //int n=Integer.parseInt(br.readLine());
+        //Scanner sc=new Scanner(System.in);
+        //int n=sc.nextInt();
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+    
+    public static void dfs(int depth,int start){
+        if(depth==m){
+            for(int i:mem)
+                sb.append(i).append(" ");
+            sb.append("\n");
+            return;
+        }
+        
+        for(int i=start;i<n;i++){
+            mem[depth]=arr[i];
+            dfs(depth+1,i);
+        }
+    }
+}
