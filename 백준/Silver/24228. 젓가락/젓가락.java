1e97a924c33044eeacad8e3b48814e8cd6d9d1a2
kivccc <71814049+kivccc@users.noreply.github.com>
2025-07-12 19:40:49 +0900

    [Silver V] Title: 젓가락, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        long n=Long.parseLong(st.nextToken());
+        long r=Long.parseLong(st.nextToken());
+            System.out.println(n+r*2-1);
+        
+
+        //int n=Integer.parseInt(br.readLine());
+        //Scanner sc=new Scanner(System.in);
+        //int n=sc.nextInt();
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+}
