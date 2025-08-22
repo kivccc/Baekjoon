43e14deed1c7a5cc04b8d46d54ccbffe78ed1999
kivccc <71814049+kivccc@users.noreply.github.com>
2025-08-22 18:51:21 +0900

    [Silver II] Title: 부분수열의 합, Time: 116 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,42 +1,35 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-    static int n, s, answer = 0;
+    static int answer=0;
+    static int n;
+    static int s;
     static int[] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        s = Integer.parseInt(st.nextToken());
-        arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        for(int i=1;i<=n;i++){
-            int[]a=new int[i];
-            dfs(0,i,a,0);
-        }
-        System.out.println(answer);
-    }
+	public static void main(String[] args) throws IOException {
 
-    
-    public static void dfs(int depth, int target,int[] result,int start) {
-        if (depth == target) {
-            long sum=0;
-            for(int i:result)
-                sum+=i;
-            if(sum==s){
-                //System.out.println(Arrays.toString(result));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        n=Integer.parseInt(st.nextToken());
+        s=Integer.parseInt(st.nextToken());
+        arr=new int[n];       
+        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        dfs(0,0,0);
+        System.out.println(answer);
+        //int n=Integer.parseInt(br.readLine());
+        //Scanner sc=new Scanner(System.in);
+        //int n=sc.nextInt();
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+    public static void dfs(int idx,int sum,int cnt){
+        if(idx==n){
+            if(cnt>0 && sum==s)
                 answer++;
-            }
             return;
         }
-
-        for(int i=start;i<n;i++){
-            result[depth]=arr[i];
-            dfs(depth+1,target,result,i+1);
-        }
+        dfs(idx+1,sum+arr[idx],cnt+1);
+        dfs(idx+1,sum,cnt);
     }
 }
