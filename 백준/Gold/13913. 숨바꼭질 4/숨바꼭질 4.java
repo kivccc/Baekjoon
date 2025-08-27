fc246c7fd6ac669e9dd91d5c79777af81e73600a
kivccc <71814049+kivccc@users.noreply.github.com>
2025-08-27 17:09:51 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 936 ms, Memory: 42140 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int K=sc.nextInt();
+        if(N==K){
+            System.out.println(0);
+            System.out.println(N);
+            return;
+        }
+        int[] mem=new int[100001];
+        Arrays.fill(mem,100001);
+        Deque<int[]> q=new ArrayDeque<>();
+        q.addLast(new int[]{N,0});
+        mem[N]=N;
+        while(!q.isEmpty()){
+            int[] arr=q.poll();
+            int cur=arr[0];
+            if(cur==K){
+                System.out.println(arr[1]);
+                ArrayList<Integer> answer=new ArrayList<>();
+                int before=cur;
+                while(before!=N){
+                    answer.add(before);
+                    before=mem[before];
+                }
+                answer.add(N);
+                for(int i=answer.size()-1;i>=0;i--)
+                    System.out.print(answer.get(i)+" ");
+                break;
+            }
+            else{
+                if(cur+1<=100000 && mem[cur+1]==100001){
+                    q.addLast(new int[]{cur+1,arr[1]+1});
+                    mem[cur+1]=cur;
+                }
+                if(0<=cur-1 && mem[cur-1]==100001){
+                    q.addLast(new int[]{cur-1,arr[1]+1});
+                    mem[cur-1]=cur;
+                }
+                if(cur*2<=100000 && mem[cur*2]==100001){
+                    q.addLast(new int[]{cur*2,arr[1]+1});
+                    mem[cur*2]=cur;
+                }
+            }
+        }
+        
+
+        //StringTokenizer st=new StringTokenizer(br.readLine());
+        //int N=Integer.parseInt(st.nextToken());
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+}
