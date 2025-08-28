6a65f7b3eef070905d1cc6f68f3f4bb5610c1906
kivccc <71814049+kivccc@users.noreply.github.com>
2025-08-28 15:34:04 +0900

    [Gold IV] Title: 팰린드롬?, Time: 760 ms, Memory: 220604 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,58 +1,42 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int[] arr=new int[n+1];
-        for(int i=1;i<=n;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
-        }
-        
-        boolean[][] dp=new boolean[n+1][n+1];
-        
-        int m=Integer.parseInt(br.readLine());
-        
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<m;i++){
-            st=new StringTokenizer(br.readLine());
-            int start=Integer.parseInt(st.nextToken());
-            int end=Integer.parseInt(st.nextToken());
-            if(arr[start]!=arr[end]){
-                sb.append(0).append("\n");
-                continue;
-            }
-            
-            int left=start+1;
-            int right=end-1;
-            boolean flag=true;
-            while(left<=right){
+        int n = Integer.parseInt(br.readLine());
 
-                if(dp[left][right]) break;
-                
-                if(arr[left]!=arr[right]){
-                    sb.append(0).append("\n");
-                    flag=false;
-                    break;
+        int[] arr = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=1; i<=n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean[][] dp = new boolean[n+1][n+1];
+
+        for (int i=1; i<=n; i++) 
+            dp[i][i] = true;
+
+        for (int i=1; i<n; i++) 
+            if (arr[i] == arr[i+1]) dp[i][i+1] = true;
+
+        for (int len=3; len<=n; len++) {
+            for (int start=1; start+len-1<=n; start++) {
+                int end = start+len-1;
+                if (arr[start] == arr[end] && dp[start+1][end-1]) {
+                    dp[start][end] = true;
                 }
-                
-                left++;
-                right--;
-            }
-            if(flag){
-                dp[start][end]=true;
-                sb.append(1).append("\n");
             }
         }
-        System.out.println(sb);
-        
 
-        //
-        //Scanner sc=new Scanner(System.in);
-        //int n=sc.nextInt();
-        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	}
+        int m = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            sb.append(dp[s][e] ? 1 : 0).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
