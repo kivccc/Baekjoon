eb29b0916082d891c7821c4e5f2a560ec02a35f4
kivccc <71814049+kivccc@users.noreply.github.com>
2025-09-26 18:48:22 +0900

    [Gold IV] Title: 최단경로, Time: 636 ms, Memory: 107864 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,52 +1,68 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static class Node{
+    static class Node{
         int to;
         int weight;
-        public Node(int to,int weight){
-            this.to=to;
-            this.weight=weight;
+        Node(int to,int weight){
+            this.to = to;
+            this.weight = weight;
         }
     }
+    
+    
+    static ArrayList<Node>[] graph;
+    static int v;
+    static int e;
+    static int k;
+    static int[] dist;
 	public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        int v=Integer.parseInt(st.nextToken());
-        int e=Integer.parseInt(st.nextToken());
-        int start=Integer.parseInt(br.readLine());
-        ArrayList<Node>[] graph=new ArrayList[v+1];
-        for(int i=1;i<=v;i++)
-            graph[i]=new ArrayList<Node>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(br.readLine());
+        dist = new int[v+1];
+        graph = new ArrayList[v+1];
+        for(int i=0;i<=v;i++)
+            graph[i] = new ArrayList<Node>();
         
         for(int i=0;i<e;i++){
-            st=new StringTokenizer(br.readLine());
-            int from=Integer.parseInt(st.nextToken());
-            int to=Integer.parseInt(st.nextToken());
-            int weight=Integer.parseInt(st.nextToken());
-            graph[from].add(new Node(to,weight));
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[u].add(new Node(v,w));
         }
-        int[] dist=new int[v+1];
-        for(int i=1;i<=v;i++)
-            dist[i]=Integer.MAX_VALUE;
-        dist[start]=0;
-        PriorityQueue<Node>pq=new PriorityQueue<>((e1,e2)->{
+        dijk(k);
+        //int n = Integer.parseInt(br.readLine());
+        //Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+        //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+    
+    static void dijk(int start){
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[start] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((e1,e2)->{
             return e1.weight-e2.weight;
         });
         pq.add(new Node(start,0));
         
         while(!pq.isEmpty()){
-            Node cur=pq.poll();
-            int now=cur.to;
-            int nowdist=cur.weight;
-            if(nowdist>dist[now]) continue;
-            for(Node next:graph[now]){
-                if(dist[next.to]>dist[now]+next.weight){
-                    dist[next.to]=dist[now]+next.weight;
-                    pq.add(new Node(next.to,dist[next.to]));
+            Node cur = pq.poll();
+            int now = cur.to;
+            int nowDist = cur.weight;
+            if(nowDist>dist[now]) continue;
+            
+            for(Node next: graph[cur.to]){
+                if(dist[next.to] > dist[now] + next.weight){
+                    dist[next.to] = dist[now] + next.weight;
+                    pq.add(new Node(next.to, dist[next.to]));
                 }
             }
-            
+
         }
         StringBuilder sb=new StringBuilder();
         for(int i=1;i<=v;i++){
@@ -57,6 +73,5 @@ public class Main {
                 sb.append(t).append("\n");
         }
         System.out.println(sb);
-        
-	}
+    }
 }
