23ab50754b5ba80bab256d9ed051516d71138192
kivccc <71814049+kivccc@users.noreply.github.com>
2025-10-18 03:28:44 +0900

    [level 2] Title: 구명보트, Time: 11.66 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,25 +1,20 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] people, int limit) {
         Arrays.sort(people);
-        Deque<Integer> deque = new ArrayDeque<>();
-        for(int i:people){
-            deque.addLast(i);
-        }
-        int answer=0;
-        while(deque.size()>0){
-            int min=deque.peekFirst();
-            int max=deque.peekLast();
-            if(min+max<=limit){
-                deque.pollLast();
-                deque.pollFirst();
-                answer++;
+        
+        int answer = 0;
+        int left = 0;             
+        int right = people.length - 1; 
+        while (left <= right) {
+            answer++;
+            if (people[left] + people[right] <= limit) {
+                left++; 
             }
-            else{
-                deque.pollLast();
-                answer++;
-            }
+            right--;
         }
+        
         return answer;
     }
 }
